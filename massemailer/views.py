d3a27dc220155c53906e398f8e3be456a67508ce
muhsinwarfa <muhsin.warfa@gmail.com>
2019-09-17 18:35:26 +0300

    mass email send implemented

--- massemailer/views.py
+++ massemailer/views.py
@@ -7,36 +7,27 @@ from django.conf import settings
 
 def index(request):
     a = "hello muthafcukaa"
-
-    # send_mail(
-    #     'Subject here',
-    #     'Here is the message.',
-    #     'muhsin.warfa@gmail.com',
-    #     ['mwarfa@qatar.cmu.edu'],
-    #     fail_silently=False,
-    # )
     context= {
         'a': a
     }
     return render(request,'index.html',context)
+
+
+
 
 def massemail(request):
     if request.method == 'POST':
         form = MailForm(request.POST)
         if form.is_valid():
             # get value of input by user
-            # receiver = request.POST['receiver']
-            # messege 1 has two CC's
-            message1 = (
-            'broooo', 'This is message 1', settings.EMAIL_HOST_USER, ['debanif@mail-file.net', 'rixon.kendyl@golld.us'])
-            # message 2 sends directly
-            message2 = ('hii ni ya pili', 'Here is another message 2', settings.EMAIL_HOST_USER, ['kerdocilmo@enayu.com'])
-            send_mass_mail((message1, message2), fail_silently=False)
+            receiver = request.POST['receiver']
+            listofemails = receiver.split(",")
+            finaltuple = ()
+            for email in listofemails:
+                finaltuple = finaltuple + (('This general subject', 'This is message',settings.EMAIL_HOST_USER,[email]),)
+            send_mass_mail(finaltuple, fail_silently=False)
             form.save()
         return redirect('index')
     form = MailForm()
     return render(request,"emailform.html", {'form': form})
-
-
-
 
