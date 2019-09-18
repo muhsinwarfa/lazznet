ae178aacc1760a50b78a830aacf6c2633ec5b97b
muhsinwarfa <muhsin.warfa@gmail.com>
2019-09-18 23:00:35 +0300

    changed model layout and improved styling

--- massemailer/models.py
+++ massemailer/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Mail(models.Model):
-    receiver = models.TextField(null=False, blank=False)
+    receiver = models.CharField(max_length=100)
     subject =  models.CharField(max_length=100)
     body =  models.TextField(default = "Hello",null=False, blank=False)
 
